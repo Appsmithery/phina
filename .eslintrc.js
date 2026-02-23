b44efaa75d9f95086573763eafb3f4c05956c812
Alextorelli <alex@appsmithery.co>
2026-02-23 16:16:42 -0500

    Update ESLint configuration to include additional ignore patterns for node_modules, dist, and Supabase functions, enhancing linting accuracy.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: 'expo',
-  ignorePatterns: ['/dist/*'],
+  ignorePatterns: ['node_modules', 'dist', 'supabase/functions'],
 };
