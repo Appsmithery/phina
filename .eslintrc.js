f1a672fa653524ff75d10298d2526a7c8c0066c5
Alextorelli <alex@appsmithery.co>
2026-02-24 11:20:05 -0500

    Add ESLint override for service worker file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,11 @@
 module.exports = {
   extends: 'expo',
   ignorePatterns: ['node_modules', 'dist', 'supabase/functions'],
+  overrides: [
+    {
+      files: ['public/sw.js'],
+      env: { browser: true },
+      globals: { self: 'readonly', URL: 'readonly' },
+    },
+  ],
 };
